83c2bbb11be14f2ed455c825205296e1b59df63c
Afonso Raposo <afonsocraposo@gmail.com>
2020-05-19 01:36:32 +0100

    Started using timer for processing timing

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:wakelock/wakelock.dart';
@@ -11,16 +13,18 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageView extends State<HomePage> with SingleTickerProviderStateMixin {
-  bool _toggled = false;
-  bool _processing = false;
-  List<SensorValue> _data = [];
+  bool _toggled = false; // toggle button value
+  List<SensorValue> _data = List<SensorValue>(); // array to store the values
   CameraController _controller;
-  double _alpha = 0.3;
+  double _alpha = 0.3; // factor for the mean value
   AnimationController _animationController;
   double _iconScale = 1;
-  int _bpm = 0;
-  int ts = 30;
-  int windowLen = 30 * 2;
+  int _bpm = 0; // beats per minute
+  int _fs = 30; // sampling frequency (fps)
+  int _windowLen = 30 * 6; // window length to display - 6 seconds
+  CameraImage _image; // store the last camera image
+  double _avg; // store the average value during calculation
+  DateTime _now; // store the now Datetime
 
   @override
   void initState() {
@@ -35,120 +39,10 @@ class HomePageView extends State<HomePage> with SingleTickerProviderStateMixin {
       });
   }
 
-  _toggle() {
-    _initController().then((onValue) {
-      Wakelock.enable();
-      _animationController.repeat(reverse: true);
-      setState(() {
-        _toggled = true;
-        _processing = false;
-      });
-      _updateBPM();
-    });
-  }
-
-  _untoggle() {
-    _disposeController();
-    Wakelock.disable();
-    _animationController.stop();
-    _animationController.value = 0.0;
-    setState(() {
-      _toggled = false;
-      _processing = false;
-    });
-  }
-
-  Future<void> _initController() async {
-    try {
-      List _cameras = await availableCameras();
-      _controller = CameraController(_cameras.first, ResolutionPreset.low);
-      await _controller.initialize();
-      Future.delayed(Duration(milliseconds: 500)).then((onValue) {
-        _controller.flash(true);
-      });
-      _controller.startImageStream((CameraImage image) {
-        if (!_processing) {
-          setState(() {
-            _processing = true;
-          });
-          _scanImage(image);
-        }
-      });
-    } catch (Exception) {
-      print(Exception);
-    }
-  }
-
-  _updateBPM() async {
-    List<SensorValue> _values;
-    double _avg;
-    int _n;
-    double _m;
-    double _threshold;
-    double _bpm;
-    int _counter;
-    int _previous;
-    while (_toggled) {
-      _values = List.from(_data);
-      _avg = 0;
-      _n = _values.length;
-      _m = 0;
-      _values.forEach((SensorValue value) {
-        _avg += value.value / _n;
-        if (value.value > _m) _m = value.value;
-      });
-      _threshold = (_m + _avg) / 2;
-      _bpm = 0;
-      _counter = 0;
-      _previous = 0;
-      for (int i = 1; i < _n; i++) {
-        if (_values[i - 1].value < _threshold &&
-            _values[i].value > _threshold) {
-          if (_previous != 0) {
-            _counter++;
-            _bpm += 60 *
-                1000 /
-                (_values[i].time.millisecondsSinceEpoch - _previous);
-          }
-          _previous = _values[i].time.millisecondsSinceEpoch;
-        }
-      }
-      if (_counter > 0) {
-        _bpm = _bpm / _counter;
-        setState(() {
-          this._bpm = ((1 - _alpha) * _bpm + _alpha * _bpm).toInt();
-        });
-      }
-      await Future.delayed(
-          Duration(milliseconds: (1000 * windowLen / ts).round()));
-    }
-  }
-
-  _scanImage(CameraImage image) {
-    double _avg =
-        image.planes.first.bytes.reduce((value, element) => value + element) /
-            image.planes.first.bytes.length;
-    if (_data.length >= windowLen) {
-      _data.removeAt(0);
-    }
-    setState(() {
-      _data.add(SensorValue(DateTime.now(), _avg));
-    });
-    Future.delayed(Duration(milliseconds: 1000 ~/ ts)).then((onValue) {
-      setState(() {
-        _processing = false;
-      });
-    });
-  }
-
-  _disposeController() {
-    _controller.dispose();
-    _controller = null;
-  }
-
   @override
   void dispose() {
-    _disposeController();
+    _untoggle();
+    _animationController.dispose();
     super.dispose();
   }
 
@@ -173,6 +67,8 @@ class HomePageView extends State<HomePage> with SingleTickerProviderStateMixin {
                             Radius.circular(18),
                           ),
                           child: Stack(
+                            fit: StackFit.expand,
+                            alignment: Alignment.center,
                             children: <Widget>[
                               _controller != null && _toggled
                                   ? AspectRatio(
@@ -216,9 +112,7 @@ class HomePageView extends State<HomePage> with SingleTickerProviderStateMixin {
                             style: TextStyle(fontSize: 18, color: Colors.grey),
                           ),
                           Text(
-                            (_bpm > 30 && _bpm < 150
-                                ? _bpm.round().toString()
-                                : "--"),
+                            (_bpm > 30 && _bpm < 150 ? _bpm.toString() : "--"),
                             style: TextStyle(
                                 fontSize: 32, fontWeight: FontWeight.bold),
                           ),
@@ -264,5 +158,134 @@ class HomePageView extends State<HomePage> with SingleTickerProviderStateMixin {
         ),
       ),
     );
+  }
+
+  void _clearData() {
+    // create array of 128 ~= 255/2
+    int now = DateTime.now().millisecondsSinceEpoch;
+    for (int i = 0; i < _windowLen; i++)
+      _data[_data.length - 1 - i] = SensorValue(
+          DateTime.fromMillisecondsSinceEpoch(now - i * 1000 ~/ _fs), 128);
+  }
+
+  void _toggle() {
+    _clearData();
+    _initController().then((onValue) {
+      Wakelock.enable();
+      _animationController?.repeat(reverse: true);
+      _initTimer();
+      setState(() {
+        _toggled = true;
+      });
+      _updateBPM(); // after is toggled
+    });
+  }
+
+  void _untoggle() {
+    _disposeController();
+    Wakelock.disable();
+    _animationController?.stop();
+    _animationController?.value = 0.0;
+    setState(() {
+      _toggled = false;
+    });
+  }
+
+  void _disposeController() {
+    _controller?.dispose();
+    _controller = null;
+  }
+
+  Future<void> _initController() async {
+    try {
+      List _cameras = await availableCameras();
+      _controller = CameraController(_cameras.first, ResolutionPreset.low);
+      await _controller.initialize();
+      Future.delayed(Duration(milliseconds: 100)).then((onValue) {
+        _controller.flash(true);
+      });
+      _controller.startImageStream((CameraImage image) {
+        _image = image;
+      });
+      _initTimer();
+    } catch (Exception) {
+      debugPrint(Exception);
+    }
+  }
+
+  void _initTimer() {
+    Timer.periodic(Duration(milliseconds: 1000 ~/ _fs), (timer) {
+      if (_toggled) {
+        if (_image != null) _scanImage(_image);
+      } else {
+        timer.cancel();
+      }
+    });
+  }
+
+  void _scanImage(CameraImage image) {
+    _now = DateTime.now();
+    _avg =
+        image.planes.first.bytes.reduce((value, element) => value + element) /
+            image.planes.first.bytes.length;
+    if (_data.length >= _windowLen) {
+      _data.removeAt(0);
+    }
+    setState(() {
+      _data.add(SensorValue(_now, _avg));
+    });
+  }
+
+  void _updateBPM() async {
+    // Bear in mind that the method used to calculate the BPM is very rudimentar
+    // feel free to improve it :)
+
+    // Since this function doesn't need to be so "exact" regarding the time it executes,
+    // I only used the a Future.delay to repeat it from time to time.
+    // Ofc you can also use a Timer object to time the callback of this function
+    List<SensorValue> _values;
+    double _avg;
+    int _n;
+    double _m;
+    double _threshold;
+    double _bpm;
+    int _counter;
+    int _previous;
+    while (_toggled) {
+      _values = List.from(_data); // create a copy of the current data array
+      _avg = 0;
+      _n = _values.length;
+      _m = 0;
+      _values.forEach((SensorValue value) {
+        _avg += value.value / _n;
+        if (value.value > _m) _m = value.value;
+      });
+      _threshold = (_m + _avg) / 2;
+      _bpm = 0;
+      _counter = 0;
+      _previous = 0;
+      for (int i = 1; i < _n; i++) {
+        if (_values[i - 1].value < _threshold &&
+            _values[i].value > _threshold) {
+          if (_previous != 0) {
+            _counter++;
+            _bpm += 60 *
+                1000 /
+                (_values[i].time.millisecondsSinceEpoch - _previous);
+          }
+          _previous = _values[i].time.millisecondsSinceEpoch;
+        }
+      }
+      if (_counter > 0) {
+        _bpm = _bpm / _counter;
+        print(_bpm);
+        setState(() {
+          this._bpm = ((1 - _alpha) * _bpm + _alpha * _bpm).toInt();
+        });
+      }
+      await Future.delayed(Duration(
+          milliseconds:
+              1000 * _windowLen ~/ _fs)); // wait for a new set of _data values
+    }
   }
 }
